8af52f87435eef83fe0391c4e60c35d619923d85
Poncholo Riego de Dios <poncholoriegodedios@gmail.com>
2025-02-01 15:21:49 +0800

    pasting codes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,68 @@
 <script lang="ts">
+    import CurrencyList from './CurrencyList.svelte';
+	import Header from './Header.svelte';
+	import Output from './Output.svelte';
+
+	let apiKey = 'cfa119cbaabf69fb4a42fdc1';
+	let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+
     let {name}:{
         name: string;
     } = $props();
-    let btn = $state({
-        name:"",
 
+
+    let input = $state({
+        value:``, //valueInput
+		from: ``, //fromConvert
+		to: ``, //toConvert
     });
+
+	let show = $state({
+input: "",
+
+	})
+
+</script>
+
+<script>
+	// import CurrencyList from './CurrencyList.svelte';
+	// import Header from './Header.svelte';
+	// import Output from './Output.svelte';
+
+	// let apiKey = 'cfa119cbaabf69fb4a42fdc1';
+	// let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+//	let valueInput;
+	let showInput;
+//	let fromConvert = ``;
+//	let toConvert = ``;
+	let showFrom = ``;
+	let showTo = ``;
+	let exchangeOut = 0;
+	let outputStatus = false;
+
+	async function Convert() {
+		if (isFinite(valueInput) && valueInput > 0 && fromConvert && toConvert) {
+			try {
+				const response = await fetch(`${api}${fromConvert}`);
+				const data = await response.json();
+				if (data.conversion_rates) {
+					let fromExchange = data.conversion_rates[fromConvert];
+					let toExchange = data.conversion_rates[toConvert];
+					exchangeOut = (valueInput / fromExchange) * toExchange;
+					outputStatus = true;
+					showFrom = fromConvert;
+					showTo = toConvert;
+					showInput = valueInput;
+				}
+			} catch (error) {
+				alert('Error fetching exchange rates:', error);
+			}
+		} else {
+			alert('Invalid input or currency selection.');
+		}
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,5 @@
+<!-- Header.svelte -->
+
+<div class="flex md:h-[100vh] md:w-24 items-center justify-center w-[100vw] h-[15vh]">
+	<p class=" md:absolute md:w-[29vw] md:-rotate-90 md:text-4xl text-3xl font-bold text-white">CURRENCY EXCHANGE</p>
+</div>
