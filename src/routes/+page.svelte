67435d78716cb1ba2a860a0523233543bb422600
Poncholo Riego de Dios <poncholoriegodedios@gmail.com>
2025-02-01 15:03:29 +0800

    trying svelte again if working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+    let {name}:{
+        name: string;
+    } = $props();
+    let btn = $state({
+        name:"",
+
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
